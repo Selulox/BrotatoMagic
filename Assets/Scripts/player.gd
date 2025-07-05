f490ada9b802e311a8e1f810e084a34c43c806c6
Oktay Sırmaçekici <79636703+Selulox@users.noreply.github.com>
2025-07-05 13:47:42 +0300

    levels

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 var health = 100.0
+var level_up_screen
 signal health_depleted
 
 func _physics_process(delta: float) -> void:
@@ -17,3 +18,9 @@ func _physics_process(delta: float) -> void:
 		if health <= 0.0:
 			health_depleted.emit()
 			%pinky.play("death")
+
+
+
+func _on_xp_bar_level_up() -> void:
+	level_up_screen = preload("res://Assets/Scenes/player_level_up_screen.tscn").instantiate()
+	
--- Assets/Scripts/xp_bar.gd
+++ Assets/Scripts/xp_bar.gd
@@ -1,8 +1,11 @@
 extends ProgressBar
 
+signal level_up
+
 func _process(delta: float) -> void:
 	%Xp_Bar.value = Global.player_xp
-	#Level_Up
+	Global.player_level = 1
 	if %Xp_Bar.value == %Xp_Bar.max_value:
 		%Xp_Bar.value = 0
 		Global.player_level += 1
+		level_up.emit()
